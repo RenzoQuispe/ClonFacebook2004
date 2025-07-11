d5c6ba4231f42ea179bb8826c34c32a7fac46c0b
RenzoQuispe <renzo123cd@gmail.com>
2025-07-11 12:41:43 -0500

    actualizar configuracion cors en index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,8 +8,6 @@ import friendshipRoutes from './routes/friendship.routes.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser";
 import cors from "cors";
-//import { app, server } from "./lib/socket.js";
-//import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -18,27 +16,30 @@ const app = express();
 app.use(express.json({ limit: '50mb' })); //Aumenta el límite del body a 50MB
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+// CORS para red local
+const corsOptions = {
+  origin: function (origin, callback) {
+
+    if (!origin) return callback(null, true);
+
+    if (origin.startsWith("http://localhost:5173")) return callback(null, true);
+
+    const regexRedLocal = /^http:\/\/192\.168\.1\.\d{1,3}:5173$/;
+
+    if (regexRedLocal.test(origin)) return callback(null, true);
+
+    return callback(new Error("CORS no permitido desde este origen: " + origin));
+
+  },
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/friendship",friendshipRoutes);
-/*
-// Servir el frontend en producción cuando la aplicación está en un entorno de despliegue (production).
-// En la etapa de desarrollo, en el archivo .env, estaba NODE_ENV=development
-const __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-*/
+
 app.listen(PORT, () => {
     console.log("Server ejecutandose en el puerto:" + PORT);
     connectDB();
