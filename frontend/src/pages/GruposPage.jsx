c56e4b9d0c196c4796c41f78807c89a7d984a491
RenzoQuispe <renzo123cd@gmail.com>
2025-07-11 23:39:18 -0500

    agregar Pages (vacios)

---
+++ frontend/src/pages/GruposPage.jsx
@@ -0,0 +1,17 @@
+import CardSocial from "../components/CardSocial";
+function GruposPage() {
+    return(
+        <div className="w-[1100px] flex-grow flex py-1 space-x-2 justify-center">
+            <CardSocial />
+            <div style={{ border: '2px solid #3a5898' }} className=" bg-white w-[890px] h-[500px]">
+                {/* Encabezado */}
+                <div style={{ background: '#3a5898' }} className=" text-white px-4 py-2 text-2xl">Grupos</div>
+                {/* Contenido */}
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full py-3">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+export default GruposPage;
